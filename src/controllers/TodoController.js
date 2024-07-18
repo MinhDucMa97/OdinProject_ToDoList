a099f4b2e912fd3437ba1237ad55c5b2f6fa6fd1
MinhDucMa97 <ducminh4497@gmail.com>
2024-07-18 14:01:33 -0700

    Continue working on implementing project.js

--- src/controllers/TodoController.js
+++ src/controllers/TodoController.js
@@ -2,7 +2,7 @@ import Task from "../modules/todo";
 
 export function updateTask() {}
 export function createTask(newTask, selectedProject) {
-  const taskObject = new Task(newTask);
-  selectedProject.addTask(taskObject);
-  UpdateProjectInDOM(selectedProject);
+	const taskObject = new Task(newTask);
+	selectedProject.addTask(taskObject);
+	UpdateProjectInDOM(selectedProject);
 }
--- src/modules/project.js
+++ src/modules/project.js
@@ -1,40 +1,47 @@
 import { stringInputValidation, numberInputValidation } from "./validation";
 
 export default class project {
-  constructor(title, projectIndex) {
-    this.title = title;
-    this.projectIndex = projectIndex;
-    this.taskArray = [];
-  }
+	constructor(title, projectIndex) {
+		this.title = title;
+		this.projectIndex = projectIndex;
+		this.taskArray = [];
+	}
 
-  set title(newTitle) {
-    if (stringInputValidation(newTitle)) {
-      this._title = newTitle;
-    }
-  }
+	set title(newTitle) {
+		if (stringInputValidation(newTitle)) {
+			this._title = newTitle;
+		}
+	}
 
-  set projectIndex(newProjectIndex) {
-    if (numberInputValidation(newProjectIndex)) {
-      this._projectIndex = newProjectIndex;
-    }
-  }
+	set projectIndex(newProjectIndex) {
+		if (numberInputValidation(newProjectIndex)) {
+			this._projectIndex = newProjectIndex;
+		}
+	}
 
-  get title() {
-    return this._title;
-  }
+	get title() {
+		return this._title;
+	}
 
-  get projectIndex() {
-    return this._projectIndex;
-  }
+	get projectIndex() {
+		return this._projectIndex;
+	}
 
-  addTask(task) {
-    this._taskArray.push(task);
-  }
+	addTask(task) {
+		this._taskArray.push(task);
+	}
 
-  removeTask(taskTitleToRemove) {
-    for (let task of this._taskArray) {
-      if (task.title === taskTitleToRemove) {
-      }
-    }
-  }
+	removeTask(taskTitleToRemove) {
+		for (let i = 0; i < this._taskArray.length; i++) {
+			if (this._taskArray[i].title === taskTitleToRemove) {
+				this._taskArray.splice(i, 1);
+				return;
+			}
+		}
+		console.error("Unable to find matching task to remove");
+	}
+
+	getTask() {
+		return this._taskArray;
+	}
 }
